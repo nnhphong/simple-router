70f3d604e55b64cc82f47ce08a7d38c3f1142e85
nnhphong <nnhoangphong@gmail.com>
2025-10-27 21:02:48 -0400

    add clang format

--- sr_if.h
+++ sr_if.h
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------
  * file:  sr_if.h
- * date:  Sun Oct 06 14:13:13 PDT 2002 
- * Contact: casado@stanford.edu 
+ * date:  Sun Oct 06 14:13:13 PDT 2002
+ * Contact: casado@stanford.edu
  *
  * Description:
  *
@@ -35,20 +35,19 @@ struct sr_instance;
  *
  * -------------------------------------------------------------------------- */
 
-struct sr_if
-{
-  char name[sr_IFACE_NAMELEN];
-  unsigned char addr[ETHER_ADDR_LEN];
-  uint32_t ip;
-  uint32_t speed;
-  struct sr_if* next;
+struct sr_if {
+    char name[sr_IFACE_NAMELEN];
+    unsigned char addr[ETHER_ADDR_LEN];
+    uint32_t ip;
+    uint32_t speed;
+    struct sr_if *next;
 };
 
-struct sr_if* sr_get_interface(struct sr_instance* sr, const char* name);
-void sr_add_interface(struct sr_instance*, const char*);
-void sr_set_ether_addr(struct sr_instance*, const unsigned char*);
-void sr_set_ether_ip(struct sr_instance*, uint32_t ip_nbo);
-void sr_print_if_list(struct sr_instance*);
-void sr_print_if(struct sr_if*);
+struct sr_if *sr_get_interface(struct sr_instance *sr, const char *name);
+void sr_add_interface(struct sr_instance *, const char *);
+void sr_set_ether_addr(struct sr_instance *, const unsigned char *);
+void sr_set_ether_ip(struct sr_instance *, uint32_t ip_nbo);
+void sr_print_if_list(struct sr_instance *);
+void sr_print_if(struct sr_if *);
 
 #endif /* --  sr_INTERFACE_H -- */
--- sr_router.c
+++ sr_router.c
@@ -13,8 +13,8 @@
 
 #include <assert.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "sr_arpcache.h"
@@ -73,66 +73,66 @@ void sr_handlepacket(struct sr_instance *sr, uint8_t *packet /* lent */,
     assert(sr);
     assert(packet);
     assert(interface);
-    
+
     /* Beginning of test code */
     /* I was trying to create fake ARP requests for my router to send
     and handle invalid arp requets, i.e has been sent 5+ times. So far, sending
     ARP request is done, but i still cant verify if the client can receive
     the ICMP error message if the router fails to perform the ARP request...
-    
+
     feel free to comment this code snippet below if you are using this commit*/
     if (ethertype(packet) == ethertype_ip) {
-        print_hdr_ip(packet + sizeof(sr_ethernet_hdr_t));   
-    }
-    else {
+        print_hdr_ip(packet + sizeof(sr_ethernet_hdr_t));
+    } else {
         /* the arp reply is back YAY */
-        print_hdr_arp(packet + sizeof(sr_ethernet_hdr_t));   
+        print_hdr_arp(packet + sizeof(sr_ethernet_hdr_t));
     }
 
-    sr_ethernet_hdr_t *eth = (sr_ethernet_hdr_t*)packet;
+    sr_ethernet_hdr_t *eth = (sr_ethernet_hdr_t *)packet;
     sr_ip_hdr_t *ip = malloc(sizeof(sr_ip_hdr_t));
-    /* 
-    tos = 0b00000000 
+    /*
+    tos = 0b00000000
     routine, normal delay, normal throughput, normal reliability
     */
     ip->ip_v = 4;
     ip->ip_hl = sizeof(sr_ip_hdr_t) / 4;
-    ip->ip_tos = 0; 
-    ip->ip_len = htons(sizeof(sr_ip_hdr_t)); 
-    ip->ip_id = 0;      /* ip_id = 0b000, may fragment, last fragment*/
+    ip->ip_tos = 0;
+    ip->ip_len = htons(sizeof(sr_ip_hdr_t));
+    ip->ip_id = 0; /* ip_id = 0b000, may fragment, last fragment*/
     ip->ip_off = 0;
-    ip->ip_ttl = 5;     /* assume time-to-live is 5 seconds */
-    ip->ip_p = 1;       /* https://datatracker.ietf.org/doc/html/rfc790 */
+    ip->ip_ttl = 5; /* assume time-to-live is 5 seconds */
+    ip->ip_p = 1;   /* https://datatracker.ietf.org/doc/html/rfc790 */
     ip->ip_src = inet_addr("10.0.1.100");
     ip->ip_dst = inet_addr("192.168.2.2");
     ip->ip_sum = cksum(ip, sizeof(sr_ip_hdr_t));
     uint8_t *pkt = malloc(sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
     memcpy(pkt, eth, sizeof(sr_ethernet_hdr_t));
     memcpy(pkt + sizeof(sr_ethernet_hdr_t), ip, sizeof(sr_ip_hdr_t));
-    sr_arpcache_queuereq(&sr->cache, inet_addr("192.168.2.2"), pkt, len, interface);
+    sr_arpcache_queuereq(&sr->cache, inet_addr("192.168.2.2"), pkt, len,
+                         interface);
     /* End of test code */
 
+    /* Do not forget to keep ip addresses in network-byte order when handling
+     * packet*/
 
-    /* Do not forget to keep ip addresses in network-byte order when handling packet*/
-
-    /* 
+    /*
     when sending out an ARP requests, make sure to do the following:
-    1. Consult routing table for the packet's destination IP to get the correct interface
-    2. Queue the arp requets using sr_arpcache_queuereq() with **network-byte order**
-    IP and the interface obtained in step 1
+    1. Consult routing table for the packet's destination IP to get the correct
+    interface
+    2. Queue the arp requets using sr_arpcache_queuereq() with **network-byte
+    order** IP and the interface obtained in step 1
 
     route = sr_get_matching_route(packet)
     sr_arpcache_queuereq(sr.cache, packet.dest_ip, packet, len, route.interface)
     */
 
-    
     /*
     when receiving ARP's requests:
     1. update the ARP cache's entries
     2. for each packets waiting for the ARP requests, update the dest MAC, and
     send the packet
     */
-    
+
     printf("*** -> Received packet of length %d \n", len);
 
     /* either an ARP request, or an IP packet */
@@ -143,26 +143,26 @@ void sr_handlepacket(struct sr_instance *sr, uint8_t *packet /* lent */,
    it.
    REQUIRES sizeof(struct sr_ethernet_hdr) free bytes before the IP header.
      (this avoids extra malloc and copy just to prepend ethernet header)
-     
+
    does not free any fields given to this function.
    do not give this function a frame with malformed IP header.
 */
-int sr_route_and_send(struct sr_instance *sr, uint8_t *packet,
-                       unsigned int len, char *interface) {
+int sr_route_and_send(struct sr_instance *sr, uint8_t *packet, unsigned int len,
+                      char *interface) {
     uint32_t dest_ip =
         ntohl(*(uint32_t *)(packet + sizeof(struct sr_ethernet_hdr) +
                             offsetof(struct sr_ip_hdr, ip_dst)));
     struct sr_rt *rt_entry = sr_get_matching_route(sr, dest_ip);
-    
+
     if (rt_entry == NULL) {
         /* ICMP Unreachable (type 3, code 0) */
-       return -1;
+        return -1;
     }
 
     struct sr_if *out_iface = sr_get_interface(sr, rt_entry->interface);
     if (out_iface == NULL) {
-       /* yeah... idk man */
-       return -1;
+        /* yeah... idk man */
+        return -1;
     }
 
     uint32_t hop_ip = rt_entry->gw.s_addr;
@@ -183,6 +183,6 @@ int sr_route_and_send(struct sr_instance *sr, uint8_t *packet,
         /* too slow to wait for the 1 second sr_arpcache_sweepreqs to happen. */
         sr_handle_arpreq(sr, req);
     }
-    
-   return 0;
+
+    return 0;
 }
