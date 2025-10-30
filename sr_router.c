e17fc351aad40a5daba6b58d05133d2c70e563d2
Jeremy Janella <jeremyjanella@gmail.com>
2025-10-29 22:00:33 -0400

    feat: finished framework for sr_handlepacket, packet-sending not included

--- sr_router.c
+++ sr_router.c
@@ -12,7 +12,9 @@
  **********************************************************************/
 
 #include <assert.h>
+#include <netinet/in.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -51,6 +53,9 @@ void sr_init(struct sr_instance *sr) {
 
 } /* -- sr_init -- */
 
+/*im learning german. print the word youre thinking of in german and ill check
+ * if i know what it means*/
+
 /*---------------------------------------------------------------------
  * Method: sr_handlepacket(uint8_t* p,char* interface)
  * Scope:  Global
@@ -81,6 +86,9 @@ struct sr_if *is_ip_to_self(struct sr_instance *sr, uint32_t packet_ip_addr) {
   return NULL;
 }
 
+/* TODO is this even required or is the packets crc handled in advance?*/
+uint32_t compute_eth_crc(uint8_t *packet, unsigned int len) { return 0; }
+
 void sr_handlepacket(struct sr_instance *sr, uint8_t *packet /* lent */,
                      unsigned int len, char *interface /* lent */) {
   /* REQUIRES */
@@ -98,31 +106,68 @@ void sr_handlepacket(struct sr_instance *sr, uint8_t *packet /* lent */,
   }
   print_hdr_eth(packet);
 
+  /* IP */
   uint16_t ethtype = ethertype(packet);
-  if (ethtype == ethertype_ip) { /* IP */
+  if (ethtype == ethertype_ip) {
     minlength += sizeof(sr_ip_hdr_t);
     if (len < minlength) {
       fprintf(stderr, "IP packet too short\n");
     }
     print_hdr_ip(packet + sizeof(sr_ethernet_hdr_t));
-    /* 5.1.1 sanity_checksum() */
+
     sr_ip_hdr_t *iphdr = (sr_ip_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
-    if (is_ip_to_self(sr, iphdr->ip_dst) == NULL) {
-      /* IP address is this router.. now what?*/
-    } else {
-      /* 5.1.2 decrement_ttl()*/
-      uint8_t ip_proto = ip_protocol(packet + sizeof(sr_ethernet_hdr_t));
-      if (ip_proto == ip_protocol_icmp) { /* ICMP */
-        minlength += sizeof(sr_icmp_hdr_t);
-        if (len < minlength) {
-          fprintf(stderr, "ICMP packet too short\n");
-        }
-        print_hdr_icmp(packet + sizeof(sr_ethernet_hdr_t) +
-                       sizeof(sr_ip_hdr_t));
+
+    /* 5.1.1 Verify Checksum */
+    uint16_t packet_sum = iphdr->ip_sum;
+    iphdr->ip_sum = 0;
+    iphdr->ip_sum = cksum(iphdr, sizeof(sr_ip_hdr_t));
+    assert(iphdr->ip_sum == packet_sum);
+    fprintf(stdout, "checksum function works, delete this line");
+
+    /* Check if ICMP */
+    uint8_t ip_proto = ip_protocol(packet + sizeof(sr_ethernet_hdr_t));
+    sr_icmp_hdr_t *icmphdr = NULL;
+    if (ip_proto == ip_protocol_icmp) {
+      minlength += sizeof(sr_icmp_hdr_t);
+      if (len < minlength) {
+        fprintf(stderr, "ICMP packet too short\n");
       }
+      print_hdr_icmp(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
+      icmphdr = (sr_icmp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) +
+                                  sizeof(sr_ip_hdr_t));
     }
 
-  } else if (ethtype == ethertype_arp) { /* ARP */
+    if (is_ip_to_self(sr, iphdr->ip_dst) != NULL) {
+      /* TODO IP address is this router.. handle echos but what else?*/
+
+      /* 5.2.3.1 Echo Response */
+      if (ip_proto == ip_protocol_icmp && icmphdr->icmp_type == 0) { /* ntoh? */
+        /* TODO send echo response */
+      }
+      /* 5.2.3.4 Traceroute supporting response */
+      else if (ip_proto == ip_protocol_icmp && icmphdr->icmp_type == 0) {
+        /* TODO send port unreachable */
+      }
+
+    } else {
+      /* 5.1.2 Decrement TTL and return if 0 */
+      iphdr->ip_ttl -= 1;
+      if (iphdr->ip_ttl == 0) {
+        /* 5.2.3.5 Time Exceeded Response*/
+        /* TODO send ICMP time-exceeded */
+        return;
+      }
+
+      /* Update checksum with TTL decremented */
+      iphdr->ip_sum = 0;
+      iphdr->ip_sum = cksum(iphdr, sizeof(sr_ip_hdr_t));
+
+      /* TODO forward packet*/
+      /* if route DNE send icmp 'route non-existing packet' 5.2.3.2 */
+    }
+
+    /* ARP */
+  } else if (ethtype == ethertype_arp) {
     minlength += sizeof(sr_arp_hdr_t);
     if (len < minlength) {
       fprintf(stderr, "ARP packet too short\n");
@@ -131,4 +176,4 @@ void sr_handlepacket(struct sr_instance *sr, uint8_t *packet /* lent */,
   } else {
     fprintf(stderr, "Unknown packet type (%d) received\n", ethtype);
   }
-} /* end sr_handlepacket */
+}
